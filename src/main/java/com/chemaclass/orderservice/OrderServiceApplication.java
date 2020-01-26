1995a8818db7634936100af59ce707ad88e4a845
Chemaclass <chemaclass@outlook.es>
2020-01-26 16:21:49 +0100

    Configure Simple state machine

--- src/main/java/com/chemaclass/orderservice/OrderServiceApplication.java
+++ src/main/java/com/chemaclass/orderservice/OrderServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
@@ -26,7 +28,7 @@ enum OrderEvents {
 }
 
 enum OrderStates {
-    SUBMIGTED,
+    SUBMITTED,
     PAID,
     FULFILLED,
     CANCELLED,
@@ -36,6 +38,29 @@ enum OrderStates {
 @Configuration
 @EnableStateMachineFactory
 class SimpleEnumStateMachineConfiguration extends StateMachineConfigurerAdapter<OrderStates, OrderEvents> {
+    @Override
+    public void configure(StateMachineTransitionConfigurer<OrderStates, OrderEvents> transitions) throws Exception {
+        transitions
+            .withExternal().source(OrderStates.SUBMITTED).target(OrderStates.PAID).event(OrderEvents.PAY)
+            .and()
+            .withExternal().source(OrderStates.PAID).target(OrderStates.FULFILLED).event(OrderEvents.FULFILL)
+            .and()
+            .withExternal().source(OrderStates.PAID).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL)
+            .and()
+            .withExternal().source(OrderStates.FULFILLED).target(OrderStates.CANCELLED).event(OrderEvents.CANCEL)
+        ;
+    }
+
+    @Override
+    public void configure(StateMachineStateConfigurer<OrderStates, OrderEvents> states) throws Exception {
+        states
+            .withStates()
+            .initial(OrderStates.SUBMITTED)
+            .state(OrderStates.PAID)
+            .state(OrderStates.FULFILLED)
+            .state(OrderStates.CANCELLED)
+        ;
+    }
 
     @Override
     public void configure(StateMachineConfigurationConfigurer<OrderStates, OrderEvents> config) throws Exception {
@@ -50,7 +75,6 @@ class SimpleEnumStateMachineConfiguration extends StateMachineConfigurerAdapter<
         config
             .withConfiguration()
             .autoStartup(false)
-            .listener(adapter)
-        ;
+            .listener(adapter);
     }
 }
