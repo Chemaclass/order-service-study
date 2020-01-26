4a0ef03b610a8dd43c67ed1f9117a087e40ec096
Chemaclass <chemaclass@outlook.es>
2020-01-26 17:44:32 +0100

    using JPA to store Orders into the db

--- src/main/java/com/chemaclass/orderservice/OrderServiceApplication.java
+++ src/main/java/com/chemaclass/orderservice/OrderServiceApplication.java
@@ -1,12 +1,18 @@
 package com.chemaclass.orderservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.StateMachineFactory;
@@ -15,7 +21,18 @@ import org.springframework.statemachine.config.builders.StateMachineStateConfigu
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
+import org.springframework.statemachine.transition.Transition;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 public class OrderServiceApplication {
@@ -42,28 +59,131 @@ enum OrderStates {
 @Log
 @Component
 class Runner implements ApplicationRunner {
-    private final StateMachineFactory<OrderStates, OrderEvents> factory;
+    private final OrderService orderService;
 
-    Runner(StateMachineFactory<OrderStates, OrderEvents> factory) {
-        this.factory = factory;
+    Runner(OrderService orderService) {
+        this.orderService = orderService;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var orderId = 12345L;
-        var machine = this.factory.getStateMachine(Long.toString(orderId));
-        machine.getExtendedState().getVariables().put("orderId", orderId);
-        machine.start();
-        log.info("INIT state: " + machine.getState().getId().name());
-        machine.sendEvent(OrderEvents.PAY);
-        log.info("Current state: " + machine.getState().getId().name());
+        var order = orderService.create(new Date());
+        log.info("After calling create()");
 
-        var events = MessageBuilder
-            .withPayload(OrderEvents.FULFILL)
-            .setHeader("name", "value")
+        var paid = orderService.pay(order.getId(), UUID.randomUUID().toString());
+        log.info("After calling pay()" + paid.getState().getId().name());
+
+        var fulfilled = orderService.fulfill(order.getId());
+        log.info("After calling fulfill()" + fulfilled.getState().getId().name());
+
+
+    }
+}
+
+@Entity(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date datetime;
+    private String state;
+
+    Order(Date d, OrderStates os) {
+        this.datetime = d;
+        this.state = os.name();
+    }
+
+    public OrderStates getOrderState() {
+        return OrderStates.valueOf(this.state);
+    }
+
+    public void setOrderState(OrderStates s) {
+        this.state = s.name();
+    }
+}
+
+interface OrderRepository extends JpaRepository<Order, Long> {
+}
+
+@Service
+class OrderService {
+    public static final String ORDER_ID_HEADER = "orderId";
+    private final OrderRepository repository;
+    private final StateMachineFactory<OrderStates, OrderEvents> factory;
+
+    OrderService(OrderRepository repository, StateMachineFactory<OrderStates, OrderEvents> factory) {
+        this.repository = repository;
+        this.factory = factory;
+    }
+
+    public Order create(Date when) {
+        return repository.save(new Order(when, OrderStates.SUBMITTED));
+    }
+
+    public StateMachine<OrderStates, OrderEvents> change(Long orderId, OrderEvents events) {
+        var machine = this.build(orderId);
+        // TODO: any logic here...
+        return machine;
+    }
+
+    public StateMachine<OrderStates, OrderEvents> fulfill(Long orderId) {
+        var machine = this.build(orderId);
+        var fulfillmentMessage = MessageBuilder.withPayload(OrderEvents.FULFILL)
+            .setHeader(ORDER_ID_HEADER, orderId)
             .build();
-        machine.sendEvent(events);
-        log.info("Current state: " + machine.getState().getId().name());
+        machine.sendEvent(fulfillmentMessage);
+
+        return machine;
+    }
+
+    public StateMachine<OrderStates, OrderEvents> pay(Long orderId, String paymentConfirmationNumber) {
+        var machine = this.build(orderId);
+        var paymentMessage = MessageBuilder.withPayload(OrderEvents.PAY)
+            .setHeader(ORDER_ID_HEADER, orderId)
+            .setHeader("paymentConfirmationNumber", paymentConfirmationNumber)
+            .build();
+        machine.sendEvent(paymentMessage);
+
+        return machine;
+    }
+
+    private StateMachine<OrderStates, OrderEvents> build(Long orderId) {
+        Order order = this.repository.findById(orderId).get();
+        var orderIdKey = Long.toString(orderId);
+        var machine = this.factory.getStateMachine(orderIdKey);
+        machine.stop();
+        machine
+            .getStateMachineAccessor()
+            .doWithAllRegions(sma -> {
+                sma.addStateMachineInterceptor(new StateMachineInterceptorAdapter<OrderStates, OrderEvents>() {
+                    @Override
+                    public void preStateChange(
+                        State<OrderStates, OrderEvents> state,
+                        Message<OrderEvents> message,
+                        Transition<OrderStates, OrderEvents> transition,
+                        StateMachine<OrderStates, OrderEvents> stateMachine
+                    ) {
+                        Optional.ofNullable(message)
+                            .flatMap(msg -> Optional.ofNullable((Long) msg.getHeaders().getOrDefault(ORDER_ID_HEADER, 1L)))
+                            .ifPresent(orderId -> {
+                                Order order1 = repository.findById(orderId).get();
+                                order1.setOrderState(state.getId());
+                                repository.save(order1);
+                            });
+                    }
+                });
+                sma.resetStateMachine(
+                    new DefaultStateMachineContext<OrderStates, OrderEvents>(
+                        order.getOrderState(), null, null, null
+                    ));
+            });
+
+        machine.start();
+
+        return machine;
     }
 }
 
